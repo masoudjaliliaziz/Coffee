3c5439f6fdba81c7b7337e163f575969f31abeb3
masoudjaliliaziz <masoudjalili0781@gmail.com>
2025-03-06 00:05:10 +0330

    add  some styling to the each product page and add counter and price of each product

--- app/_component/form/OrderForm.tsx
+++ app/_component/form/OrderForm.tsx
@@ -9,6 +9,7 @@ type Props = {
 
 function OrderForm({ product }: Props) {
   const [roast, setRoast] = useState("");
+  const [counter, setCounter] = useState(1);
   console.log(roast);
   const roastData = [
     { title: "لایت", value: "light" },
@@ -24,12 +25,12 @@ function OrderForm({ product }: Props) {
   const machineData = [
     { title: "دستگاه خانگی", value: "home" },
     { title: "فرنج پرس", value: "french" },
-    { title: "موکوپات", value: "moko" },
+    { title: "موکاپات", value: "moko" },
     { title: "دستگاه صنعتی", value: "main" },
   ];
   return (
     <form className="w-full h-full  py-6 flex flex-col gap-1.5 ">
-      <div className="flex   w-full py-2 justify-start gap-6 bg-base-200 rounded-md  ">
+      <div className="flex min-h-2/3   w-full py-2 justify-start gap-6 bg-base-200 rounded-md  ">
         <h1 className="text-xs font-bold rounded-md  p-1 badge badge-neutral w-1/6  ">
           میزان رست
         </h1>
@@ -74,6 +75,34 @@ function OrderForm({ product }: Props) {
               {data.title}
             </div>
           ))}
+        </div>
+      </div>
+      <div className="w-full min-h-1/3  flex my-5">
+        <div className="w-2/3 h-full  flex gap-3 items-center">
+          <h1 className="font-bold text-xs">تعداد</h1>
+          <div className="flex justify-center items-center gap-1 bg-base-300 px-3 py-1 rounded-lg">
+            <div
+              className="cursor-pointer p-0.5 bg-base-300  rounded-md w-4 h-4 flex justify-center items-center"
+              onClick={() => setCounter((cur) => cur + 1)}
+            >
+              +
+            </div>
+            <div className=" text-sm font-black">{counter}</div>
+            <div
+              className="cursor-pointer p-0.5 bg-base-300  rounded-md w-4 h-4 flex justify-center items-center"
+              onClick={() =>
+                setCounter((cur) => (cur === 1 ? (cur = 1) : cur - 1))
+              }
+            >
+              -
+            </div>
+          </div>
+        </div>
+        <div className=" w-1/3 h-full  items-center flex gap-2">
+          <h1 className="text-xs font-semibold">قیمت نهایی</h1>
+          <div className="text-xs font-bold flex gap-3 px-1.5 py-1 rounded-lg bg-base-300">
+            {product.price * counter}هزار تومن
+          </div>
         </div>
       </div>
     </form>
